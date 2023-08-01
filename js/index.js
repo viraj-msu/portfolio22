750433fe683acb88f7475fac32925684637c4c77
Viraj <virajdhimmar.msu@gmail.com>
2023-08-02 02:24:08 +0530

    commited

---
+++ js/index.js
@@ -0,0 +1,18 @@
+let abc = document.getElementById('icon');
+
+// icon.onclick = function(){
+//     console.log(" hey from fn")
+// };
+
+
+abc.addEventListener('click', function() {
+  console.log(" hey from fn");
+  icon.style.backgroundColor="blue";
+});
+
+// document.body.className.toggle('lightTheme');
+
+console.log(" hey")
+
+
+
