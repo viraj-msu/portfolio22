3a3680272bf52ff94db2c1170ba8abf2fc6e7934
Viraj <virajdhimmar.msu@gmail.com>
2023-08-02 03:47:11 +0530

    commit

--- js/index.js
+++ js/index.js
@@ -6,13 +6,33 @@ let abc = document.getElementById('icon');
 
 
 abc.addEventListener('click', function() {
-  console.log(" hey from fn");
-  icon.style.backgroundColor="blue";
+  document.body.classList.toggle('lightTheme');
+
+  // document.getElementById("on").style.display="none";
+  // document.getElementById("off").style.display="auto";
+
+  // document.getElementById("light").classList.toggle = "light";
 });
 
-// document.body.className.toggle('lightTheme');
+// abc.addEventListener('click', function() {
+//   console.log(" hey from fn");
+//   document.body.className.toggleClass('lightTheme');
+// });
 
-console.log(" hey")
+
+// const body = document.getElementById("body")
+// const switchBTN = document.getElementById("icon")
 
 
+// function clickEvent() {
+//   console.log(" hey");
+//   if(body.classList('lightTheme')) {
+//     body.classList.remove('lightTheme')
+//     body.classList.add('root')
+//   } else {
+//     body.classList.remove('root')
+//     body.classList.add('lightTheme')
+//   }
+// }
 
+// switchBTN.addEventListener('click', clickEvent)
